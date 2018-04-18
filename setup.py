8ef63068c61819266fdea64af504043ad9f831ec
Myle Ott <myleott@fb.com>
2018-04-18 07:04:20 -0600

    Use dependency_links for installation

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages, Extension
+from setuptools import setup, find_packages
 
 def readme():
     with open('README.md') as f:
@@ -7,15 +7,6 @@ def readme():
 def requirements():
     with open('requirements.txt') as f:
         return f.read()
-
-bleu = Extension(
-    'fairseq.libbleu',
-    sources=[
-        'fairseq/fairseq/clib/libbleu/libbleu.cpp',
-        'fairseq/fairseq/clib/libbleu/module.cpp',
-    ],
-    extra_compile_args=['-std=c++11'],
-)
 
 setup(
     name='fbtranslate',
@@ -26,6 +17,10 @@ setup(
     url='https://github.com/facebookincubator/fbtranslate',
     license='BSD',
     packages=find_packages(),
-    ext_modules=[bleu],
-    install_requires=['fairseq'],
+    install_requires=[
+        'fairseq',
+    ],
+    dependency_links=[
+        "git+https://github.com/facebookresearch/fairseq-py.git@d3795d6cd1c66ac05dc0f4861ce69ab4680bff3d#egg=fairseq-0.4.0"
+    ],
 )
