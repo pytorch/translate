0357d3d3d5743a56953d4b4e94e279d5bbff3ca7
Juan Miguel Pino <juancarabina@fb.com>
2018-04-24 08:48:26 -0700

    Changes done internally at Facebook
    735738eb5f79 juancarabina Sync OS to fbcode
    32b1414b192e jcross averaged SGD per checkpoint
    fc3cfa799ba3 tomdzk Fix tests that rely on projects-sources
    a99fff5506f7 weiho Refactor to guarantee consistent usage of flags related to appending-EOS and reversing-source
    5fa3a76f25e8 lie [fbtranslate] Evaluation workflow for FBTranslate's generate.py
    595cc791f5f0 jcross onnx-export refactor to share code
    a5d2f3ace428 lie [fbtranslate] Remove fairseq data loading and disallow inferring corpus file paths
    c2b15ac249af jcross remove Variable from fbtranslate
    a489d15d1760 lie [fbtranslate] Build vocabs from train corpora before launching the train clones
    6e91b3d160ce lie [fbtranslate] Remove fairseq data loading support in generate.py
    fcffee454fea orionr [caffe2] Update to github:62ac7f981271609adaf2220d5dcecab84d71b2ce
    565b36ec93d7 juancarabina Scripts to sync fbtranslate
    71218a5c9594 orionr [pytorch][caffe2] Move internal references from pytorch/pytorch to caffe2
    0ab1e93c134c jcross batched beam step for onnx export
    2965c332a3a8 jcross avoid F.linear with 3D input b/c/o hidden transpose
    afb1d805a03c lie [fbtranslate] Remove extra_model_args override in generate.py
    109a3bc170c3 juancarabina Add quantization option to ONNX export script
    04ae3a459e2b juancarabina [Easy] Exclude vocab from github sync
    332061f3577c juancarabina [Easy][fbtranslate] Update to github:4b8fc369e21df524d0bdb361872ff702f568c226
    9e7c6dd08f74 weiho Simple Dictionary class for OSS FBTranslate
    dfd26107032e juancarabina more exclude

---
+++ fbtranslate/test/DictionaryTest.cpp
@@ -0,0 +1,90 @@
+#include "language_technology/neural_mt/fbtranslate/Dictionary.h"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <exception>
+#include <fstream>
+#include <string>
+#include <vector>
+
+#include "folly/experimental/TestUtil.h"
+
+using ::folly::test::TemporaryFile;
+
+namespace pytorch {
+namespace translate {
+
+namespace {
+static TemporaryFile writeToTemp(const std::string& content) {
+  TemporaryFile tempFile = TemporaryFile("test_temp");
+  std::ofstream temp_stream(tempFile.path().string());
+  temp_stream << content;
+  return tempFile;
+}
+} // anonymous namespace
+
+TEST(DictionaryTest, SpecialIdsTest) {
+  TemporaryFile tempFile = writeToTemp("foo 42");
+  Dictionary dict(tempFile.path().string());
+  EXPECT_EQ(kPadId, dict.padId());
+  EXPECT_EQ(kEosId, dict.eosId());
+  EXPECT_EQ(kUnkId, dict.unkId());
+}
+
+TEST(DictionaryTest, TokenizeTest) {
+  std::string line = "Ce   n'est pas \t une\nphrase!  ";
+  std::vector<std::string> expected_tokens = {
+      "Ce", "n'est", "pas", "une", "phrase!"};
+  std::vector<std::string> tokens = Dictionary::tokenize(line);
+  EXPECT_THAT(tokens, testing::ElementsAreArray(expected_tokens));
+}
+
+TEST(DictionaryTest, NumberizeAndDenumberizeTest) {
+  TemporaryFile tempFile = writeToTemp(R"(foo 42
+bar 30
+zoohar 777
+moo boo 0
+)");
+  Dictionary dict(tempFile.path().string());
+
+  // Tests numberize().
+  std::vector<std::string> tokens = {
+      "hi",
+      "foo",
+      "today",
+      "was",
+      "moo boo",
+  };
+  std::vector<int> expected_ids{
+      kUnkId,
+      kMaxSpecialTokens,
+      kUnkId,
+      kUnkId,
+      kMaxSpecialTokens + 3,
+  };
+  std::vector<int> ids = dict.numberize(tokens);
+  EXPECT_THAT(ids, testing::ElementsAreArray(expected_ids));
+
+  // Tests denumberize().
+  ids = {
+      kMaxSpecialTokens,
+      kMaxSpecialTokens + 3,
+      dict.size(),
+      kMaxSpecialTokens + 2,
+  };
+  std::vector<std::string> expected_tokens{
+      "foo",
+      "moo boo",
+      kUnkSymbol,
+      "zoohar",
+  };
+  tokens = dict.denumberize(ids);
+  EXPECT_THAT(tokens, testing::ElementsAreArray(expected_tokens));
+}
+
+TEST(DictionaryTest, ExceptionTest) {
+  EXPECT_THROW(Dictionary dict("/non/existent/file"), std::invalid_argument);
+}
+
+} // namespace translate
+} // namespace pytorch
