9b3eea8fb7bfbe0d02c93bc41e919cdb0b826ea9
James Cross <james.henry.cross.iii@gmail.com>
2018-04-29 11:55:53 -0700

    build cpp (example code) in install.sh

--- install.sh
+++ install.sh
@@ -8,9 +8,42 @@ rm miniconda.sh
 popd
 
 . ~/miniconda/bin/activate
-conda uninstall -y pytorch pytorch-nightly
-conda install -y pytorch-nightly -c pytorch
 
-. ~/miniconda/bin/activate
+export CONDA_PATH="$(dirname $(which conda))/../" # [anaconda root directory]
+
+# Install basic PyTorch dependencies
+conda install numpy pyyaml mkl mkl-include setuptools cmake cffi typing
+
+# Add LAPACK support for the GPU
+conda install -c pytorch magma-cuda80 # or magma-cuda90 if CUDA 9
+
+pushd ~
+git clone --recursive https://github.com/pytorch/pytorch
+cd pytorch
+
+# PyTorch build from source
+python setup.py install
+
+# Caffe2 build from source (with ATen)
+#CMAKE_ARGS=-DUSE_ATEN=ON python setup_caffe2.py install
+mkdir build_caffe2 && pushd build_caffe2
+cmake -DUSE_ATEN=ON -DCMAKE_PREFIX_PATH=$CONDA_PATH -DCMAKE_INSTALL_PATH=$CONDA_PATH
+make DESTDIR=$CONDA_PATH install -j8 2>&1 | tee MAKE_OUT
+popd
+popd
+
+export LD_LIBRARY_PATH=$CONDA_PATH/lib:$LD_LIBRARY_PATH
+
+# Install ONNX
+pushd ~
+git clone --recursive https://github.com/onnx/onnx.git
+pip install ./onnx
+popd
+
 yes | pip uninstall fbtranslate
 python3 setup.py build develop
+
+pushd cpp
+cmake -DCMAKE_PREFIX_PATH==$CONDA_PATH/usr/local -DCMAKE_INSTALL_PATH==$CONDA_PATH .
+make
+popd
