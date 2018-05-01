8d06da7b32c7147e19838bfbf944700c92e06ddf
Juan Miguel Pino <juancarabina@fb.com>
2018-05-01 10:25:59 -0700

    update deps

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+export http_proxy="http://fwdproxy.any:8080"
+export https_proxy="$http_proxy"
+export no_proxy=".facebook.com,.tfbnw.net,.fb.com, .fbcdn.net"
+
 pushd ~
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 chmod +x miniconda.sh
