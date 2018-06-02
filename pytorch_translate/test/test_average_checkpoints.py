31fe23dd3a5371a4a260be5c7d23dc455b7e00f7
James Cross <jcross@fb.com>
2018-06-01 17:44:47 -0700

    black formatting for Python tests
    Summary: Applying black auto-formatting to Python test files in order to avoid creating noise on other diffs.
    
    Differential Revision: D8244964
    
    fbshipit-source-id: 80521602f1d3d34b06f64f04c19e3416f5e35ad6

--- pytorch_translate/test/test_average_checkpoints.py
+++ pytorch_translate/test/test_average_checkpoints.py
@@ -14,33 +14,33 @@ class AverageCheckpointsTest(unittest.TestCase):
     def test_average_checkpoints(self):
         params_0 = collections.OrderedDict(
             [
-                ('a', torch.DoubleTensor([100.0])),
-                ('b', torch.FloatTensor([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])),
-                ('c', torch.IntTensor([7, 8, 9])),
+                ("a", torch.DoubleTensor([100.0])),
+                ("b", torch.FloatTensor([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])),
+                ("c", torch.IntTensor([7, 8, 9])),
             ]
         )
         params_1 = collections.OrderedDict(
             [
-                ('a', torch.DoubleTensor([1.0])),
-                ('b', torch.FloatTensor([[1.0, 1.0, 1.0], [1.0, 1.0, 1.0]])),
-                ('c', torch.IntTensor([2, 2, 2])),
+                ("a", torch.DoubleTensor([1.0])),
+                ("b", torch.FloatTensor([[1.0, 1.0, 1.0], [1.0, 1.0, 1.0]])),
+                ("c", torch.IntTensor([2, 2, 2])),
             ]
         )
         params_avg = collections.OrderedDict(
             [
-                ('a', torch.DoubleTensor([50.5])),
-                ('b', torch.FloatTensor([[1.0, 1.5, 2.0], [2.5, 3.0, 3.5]])),
+                ("a", torch.DoubleTensor([50.5])),
+                ("b", torch.FloatTensor([[1.0, 1.5, 2.0], [2.5, 3.0, 3.5]])),
                 # We expect truncation for integer division
-                ('c', torch.IntTensor([4, 5, 5])),
+                ("c", torch.IntTensor([4, 5, 5])),
             ]
         )
 
         fd_0, path_0 = tempfile.mkstemp()
         fd_1, path_1 = tempfile.mkstemp()
-        torch.save(collections.OrderedDict([('model', params_0)]), path_0)
-        torch.save(collections.OrderedDict([('model', params_1)]), path_1)
+        torch.save(collections.OrderedDict([("model", params_0)]), path_0)
+        torch.save(collections.OrderedDict([("model", params_1)]), path_1)
 
-        output = average_checkpoints([path_0, path_1])['model']
+        output = average_checkpoints([path_0, path_1])["model"]
 
         os.close(fd_0)
         os.remove(path_0)
@@ -48,16 +48,17 @@ class AverageCheckpointsTest(unittest.TestCase):
         os.remove(path_1)
 
         for (k_expected, v_expected), (k_out, v_out) in zip(
-                params_avg.items(), output.items()):
+            params_avg.items(), output.items()
+        ):
             self.assertEquals(
                 k_expected,
                 k_out,
-                f'Key mismatch - expected {k_expected} but found {k_out}. '
-                '(Expected list of keys: {params_avg.keys()} '
-                'vs actual list of keys: {output.keys()})'
+                f"Key mismatch - expected {k_expected} but found {k_out}. "
+                "(Expected list of keys: {params_avg.keys()} "
+                "vs actual list of keys: {output.keys()})",
             )
             np.testing.assert_allclose(
                 v_expected.numpy(),
                 v_out.numpy(),
-                err_msg=f'Tensor value mismatch for key {k_expected}'
+                err_msg=f"Tensor value mismatch for key {k_expected}",
             )
--- pytorch_translate/test/utils.py
+++ pytorch_translate/test/utils.py
@@ -10,10 +10,9 @@ from pytorch_translate import dictionary as pytorch_translate_dictionary
 
 
 class ModelParamsDict:
-
     def __init__(
         self,
-        cell_type='lstm',
+        cell_type="lstm",
         encoder_bidirectional=False,
         encoder_freeze_embed=False,
         decoder_freeze_embed=False,
@@ -21,7 +20,7 @@ class ModelParamsDict:
         sequence_lstm=False,
     ):
         # Model params
-        self.arch = 'rnn'
+        self.arch = "rnn"
         self.encoder_embed_dim = 10
         self.encoder_freeze_embed = encoder_freeze_embed
         self.encoder_hidden_dim = 10
@@ -37,19 +36,19 @@ class ModelParamsDict:
         self.dropout = 0
         self.decoder_dropout_in = 0
         self.decoder_dropout_out = 0
-        self.attention_type = 'dot'
+        self.attention_type = "dot"
         self.residual_level = None
         self.averaging_encoder = False
         self.cell_type = cell_type
         self.sequence_lstm = sequence_lstm
         # Training params
-        self.criterion = 'cross_entropy'
+        self.criterion = "cross_entropy"
         self.lr = [0.1]
-        self.optimizer = 'sgd'
+        self.optimizer = "sgd"
         self.momentum = 0
         self.label_smoothing_epsilon = None
         self.weight_decay = 0.0
-        self.lr_scheduler = 'fixed'
+        self.lr_scheduler = "fixed"
         self.force_anneal = 0
         self.lr_shrink = 0
         self.sentence_avg = True
@@ -61,7 +60,6 @@ class ModelParamsDict:
 
 
 class TestDataset(torch.utils.data.Dataset):
-
     def __init__(self, data):
         super().__init__()
         self.data = data
@@ -73,15 +71,12 @@ class TestDataset(torch.utils.data.Dataset):
         return len(self.data)
 
 
-def dummy_dictionary(
-    dummy_tokens=3,
-    additional_token_list=None,
-):
+def dummy_dictionary(dummy_tokens=3, additional_token_list=None):
     """First adds the amount of dummy_tokens that you specify, then
     finally the additional_token_list, which is a list of string token values"""
     d = pytorch_translate_dictionary.Dictionary()
     for i in range(dummy_tokens):
-        token = f'token_{i}'
+        token = f"token_{i}"
         d.add_symbol(token)
     if additional_token_list is not None:
         for token in additional_token_list:
@@ -91,43 +86,35 @@ def dummy_dictionary(
 
 
 def prepare_inputs(
-    test_args,
-    source_vocab_size=103,
-    target_vocab_size=103,
-    is_variable_seqlen=False,
+    test_args, source_vocab_size=103, target_vocab_size=103, is_variable_seqlen=False
 ):
     # first 100 indices are reserved for special tokens
     src_dict = dummy_dictionary(dummy_tokens=source_vocab_size - 100)
     tgt_dict = dummy_dictionary(dummy_tokens=source_vocab_size - 100)
 
-    def get_single_example(
-        sample_id,
-        src_sentence_length,
-        tgt_sentence_length,
-    ):
+    def get_single_example(sample_id, src_sentence_length, tgt_sentence_length):
         non_special_start = 4
         example = {
-            'id': sample_id,
+            "id": sample_id,
             # Note: both source and target-side sentences are expected
             # to end in the EOS marker. LanguagePairDataset then:
             # (1) moves the EOS to the start of the target, for input feeding
             # (2) it also handles left (right) padding of the source (target)
-            'source': torch.LongTensor(
+            "source": torch.LongTensor(
                 np.random.randint(
                     low=non_special_start,
                     high=len(src_dict.symbols),
                     size=src_sentence_length,
                 ).tolist()
-                + [src_dict.eos()],
+                + [src_dict.eos()]
             ),
-
-            'target': torch.LongTensor(
+            "target": torch.LongTensor(
                 np.random.randint(
                     low=non_special_start,
                     high=len(tgt_dict.symbols),
                     size=tgt_sentence_length,
                 ).tolist()
-                + [tgt_dict.eos()],
+                + [tgt_dict.eos()]
             ),
         }
         return example
@@ -135,25 +122,24 @@ def prepare_inputs(
     min_sent_len = 7
     max_sent_len = 12
     fixed_tgt_length = 12
-    dataset = TestDataset([
-        get_single_example(
-            example_id,
-            np.random.randint(
-                low=min_sent_len,
-                high=max_sent_len,
-                size=1,
-            ) if is_variable_seqlen else 10,
-            fixed_tgt_length,
-        ) for example_id in range(test_args.batch_size)
-    ])
+    dataset = TestDataset(
+        [
+            get_single_example(
+                example_id,
+                np.random.randint(low=min_sent_len, high=max_sent_len, size=1)
+                if is_variable_seqlen
+                else 10,
+                fixed_tgt_length,
+            )
+            for example_id in range(test_args.batch_size)
+        ]
+    )
     dataloader = torch.utils.data.DataLoader(
         dataset,
         batch_size=test_args.batch_size,
         collate_fn=(
             lambda samples: data.LanguagePairDataset.collate(
-                samples,
-                src_dict.pad(),
-                src_dict.eos(),
+                samples, src_dict.pad(), src_dict.eos()
             )
         ),
     )
@@ -162,32 +148,30 @@ def prepare_inputs(
 
 
 def create_lexical_dictionaries():
-    lexical_dictionary_path = write_lines_to_temp_file([
-        'a A 0.7',
-        'a B 0.3',
-        'b C 0.1',
-        'b D 0.8',
-        'b E 0.1',
-        'c A 0.3',
-        'c B 0.4',
-        'c C 0.3',
-        'd D 0.4',
-        'd E 0.3',
-        'd A 0.2',
-        'd B 0.1',
-        'e C 1.0',
-    ])
+    lexical_dictionary_path = write_lines_to_temp_file(
+        [
+            "a A 0.7",
+            "a B 0.3",
+            "b C 0.1",
+            "b D 0.8",
+            "b E 0.1",
+            "c A 0.3",
+            "c B 0.4",
+            "c C 0.3",
+            "d D 0.4",
+            "d E 0.3",
+            "d A 0.2",
+            "d B 0.1",
+            "e C 1.0",
+        ]
+    )
     return [lexical_dictionary_path]
 
 
 def write_lines_to_temp_file(lines):
-    temp_file = tempfile.NamedTemporaryFile(
-        mode='w',
-        delete=False,
-        dir='/tmp'
-    )
+    temp_file = tempfile.NamedTemporaryFile(mode="w", delete=False, dir="/tmp")
     temp_file_path = temp_file.name
     temp_file.close()
-    with codecs.open(temp_file_path, 'w', 'utf-8') as temp_file:
-        temp_file.write('\n'.join(lines) + '\n')
+    with codecs.open(temp_file_path, "w", "utf-8") as temp_file:
+        temp_file.write("\n".join(lines) + "\n")
     return temp_file_path
