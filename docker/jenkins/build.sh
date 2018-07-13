4b53be6cd71e3440e08555dffe065f89e3e92fd0
Wei Ho <weiho@fb.com>
2018-07-13 15:54:39 -0700

    Set Jenkins user in Docker image
    Summary: Pull Request resolved: https://github.com/pytorch/translate/pull/154
    
    Reviewed By: ezyang
    
    Differential Revision: D8832738
    
    fbshipit-source-id: b89e8e0961daf467cc04d472f54d8cb950c0c5d1

--- docker/jenkins/build.sh
+++ docker/jenkins/build.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 # Builds Docker image.
 
+# Set Jenkins UID and GID if running Jenkins
+if [ -n "${JENKINS:-}" ]; then
+  JENKINS_UID=$(id -u jenkins)
+  JENKINS_GID=$(id -g jenkins)
+fi
+
 # ${@:2} skips the first argument since we don't have multiple
 # Docker images, as opposed to like
 # https://github.com/pietern/pytorch-dockerfiles/blob/master/build.sh
 docker build \
   --no-cache \
+  --build-arg "JENKINS=${JENKINS:-}" \
+  --build-arg "JENKINS_UID=${JENKINS_UID:-}" \
+  --build-arg "JENKINS_GID=${JENKINS_GID:-}" \
   "${@:2}" \
   .
