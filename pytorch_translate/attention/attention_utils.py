df68faa6cb82710ca156120d1caf9cf75102a36c
Paul Michel <pmichel@fb.com>
2018-07-23 08:35:25 -0700

    MLP attention (#150)
    Summary:
    This implements the MLP style attention from the original paper.
    
    This also reformats the `dot_attention` a little bit and adds unit tests for masking, dot and MLP attention
    Pull Request resolved: https://github.com/pytorch/translate/pull/150
    
    Differential Revision: D8811304
    
    Pulled By: pmichel31415
    
    fbshipit-source-id: 29ac303144399cba228e87980edbe27d3e541686

--- pytorch_translate/attention/attention_utils.py
+++ pytorch_translate/attention/attention_utils.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import torch
+import torch.nn.functional as F
+import numpy as np
 
 
 def create_src_lengths_mask(batch_size, src_lengths):
@@ -26,3 +28,16 @@ def create_src_lengths_mask(batch_size, src_lengths):
     )
     # returns [batch_size, max_seq_len]
     return (src_indices < src_lengths).int().detach()
+
+
+def masked_softmax(scores, src_lengths, src_length_masking=True):
+    """Apply source length masking then softmax.
+    Input and output have shape bsz x src_len"""
+    if src_length_masking:
+        bsz, max_src_len = scores.size()
+        # compute masks
+        src_mask = create_src_lengths_mask(bsz, src_lengths)
+        # Fill pad positions with -inf
+        scores = scores.masked_fill(src_mask == 0, -np.inf)
+
+    return F.softmax(scores, dim=-1)
---
+++ pytorch_translate/attention/mlp_attention.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+from pytorch_translate.attention import (
+    BaseAttention,
+    register_attention,
+    attention_utils,
+)
+from pytorch_translate.common_layers import Linear
+
+
+@register_attention('mlp')
+class MLPAttention(BaseAttention):
+    """The original attention from Badhanau et al. (2014)
+    https://arxiv.org/abs/1409.0473 based on a Multi-Layer Perceptron.
+
+    The attention score between position i in the encoder and position j in the
+    decoder is:
+    alpha_ij = V_a * tanh(W_ae * enc_i + W_ad * dec_j + b_a)
+    """
+
+    def __init__(self, decoder_hidden_state_dim, context_dim, **kwargs):
+        super().__init__(decoder_hidden_state_dim, context_dim)
+
+        self.context_dim = context_dim
+        self.attention_dim = kwargs.get("attention_dim", context_dim)
+        # W_ae and b_a
+        self.encoder_proj = Linear(
+            context_dim, self.attention_dim, bias=True
+        )
+        # W_ad
+        self.decoder_proj = Linear(
+            decoder_hidden_state_dim, self.attention_dim, bias=False
+        )
+        # V_a
+        self.to_scores = Linear(
+            self.attention_dim, 1, bias=False
+        )
+        self.src_length_masking = kwargs.get("src_length_masking", True)
+
+    def forward(self, decoder_state, source_hids, src_lengths):
+        """The expected input dimensions are:
+
+        decoder_state: bsz x decoder_hidden_state_dim
+        source_hids: src_len x bsz x context_dim
+        src_lengths: bsz
+        """
+        src_len, bsz, _ = source_hids.size()
+        # (src_len*bsz) x context_dim (to feed through linear)
+        flat_source_hids = source_hids.view(-1, self.context_dim)
+        # (src_len*bsz) x attention_dim
+        encoder_component = self.encoder_proj(flat_source_hids)
+        # src_len x bsz x attention_dim
+        encoder_component = encoder_component.view(src_len, bsz, self.attention_dim)
+        # 1 x bsz x attention_dim
+        decoder_component = self.decoder_proj(decoder_state).unsqueeze(0)
+        # Sum with broadcasting and apply the non linearity
+        # src_len x bsz x attention_dim
+        hidden_att = F.tanh(
+            (decoder_component + encoder_component).view(-1, self.attention_dim)
+        )
+        # Project onto the reals to get attentions scores (bsz x src_len)
+        attn_scores = self.to_scores(hidden_att).view(src_len, bsz).t()
+
+        # Mask + softmax (src_len x bsz)
+        normalized_masked_attn_scores = attention_utils.masked_softmax(
+            attn_scores, src_lengths, self.src_length_masking
+        ).t()
+
+        # Sum weighted sources (bsz x context_dim)
+        attn_weighted_context = (
+            source_hids * normalized_masked_attn_scores.unsqueeze(2)
+        ).sum(0)
+
+        return attn_weighted_context, normalized_masked_attn_scores
