1a9a984f6dda7844b2b3dc4c9e2d14f92e2d89f7
Liezl Puzon <lie@fb.com>
2018-08-27 00:48:22 -0700

    Ensembling support in generation for models with different output softmax size (#210)
    Summary:
    Pull Request resolved: https://github.com/pytorch/translate/pull/210
    
    In general, this diff enables the ensembling of multiple models with different sizes of output softmax (for example where model 1 outputs a set of 10 translation candidates + 10 probabilities for each of those candidates, and model 2 outputs a set of 20 translation candidates + 20 probabilities for each of those candidates). See documentation in comments for a full example.
    
    The context of this diff is to allow ensembling of multiple models trained with word prediction vocab reduction.
    
    Reviewed By: xianxl
    
    Differential Revision: D9504725
    
    fbshipit-source-id: 2c5747ce9d7a1bfd556fe3d9362a53e3353b43ee

--- pytorch_translate/test/test_beam_decode.py
+++ pytorch_translate/test/test_beam_decode.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import unittest
+from typing import Any, List
 
 import numpy as np
 import torch
@@ -42,3 +43,58 @@ class TestBeamDecode(unittest.TestCase):
         word_lengths = torch.LongTensor([[5, 5, 5], [5, 5, 5]])
         encoder_input = (src_tokens, src_lengths, char_inds, word_lengths)
         translator.generate(encoder_input, maxlen=7)
+
+    @unittest.skipIf(torch.cuda.device_count() < 1, "No GPU available for test.")
+    def test_gather_probs_with_vr(self):
+        """ Tests gather_probs when there is vocab reduction """
+        all_translation_tokens: List[Any] = [
+            torch.LongTensor([3, 7, 8, 9]),
+            torch.LongTensor([0, 3, 5]),
+        ]
+        all_probs: List[Any] = [
+            torch.FloatTensor(
+                [[0.25, 0.25, 0.25, 0.25], [0.25, 0.25, 0.25, 0.25]]
+            ).cuda(),
+            torch.FloatTensor([[0.4, 0.5, 0.1], [0.4, 0.5, 0.1]]).cuda(),
+        ]
+        avg_probs, possible_translation_tokens = beam_decode.SequenceGenerator.gather_probs(
+            all_translation_tokens=all_translation_tokens, all_probs=all_probs
+        )
+        avg_probs = avg_probs.detach().cpu().numpy()
+        possible_translation_tokens = possible_translation_tokens.detach().cpu().numpy()
+
+        avg_probs_ref = sorted([0.4, 0.75, 0.1, 0.25, 0.25, 0.25])
+        possible_translation_tokens_ref = sorted([0, 3, 5, 7, 8, 9])
+
+        np.testing.assert_allclose(
+            actual=np.sort(avg_probs[0]), desired=np.array(avg_probs_ref), atol=1e-5
+        )
+        np.testing.assert_allclose(
+            actual=np.sort(possible_translation_tokens),
+            desired=np.array(possible_translation_tokens_ref),
+        )
+        np.testing.assert_allclose(
+            actual=np.sort(possible_translation_tokens),
+            desired=np.array(possible_translation_tokens_ref),
+        )
+
+    @unittest.skipIf(torch.cuda.device_count() < 1, "No GPU available for test.")
+    def test_gather_probs_without_vr(self):
+        """ Tests gather_probs when there is no vocab reduction """
+        all_probs: List[Any] = [
+            torch.FloatTensor([[0.25, 0.25, 0.25, 0.25], [0.25, 0.25, 0.25, 0.25]]),
+            torch.FloatTensor([[0.4, 0.2, 0.1, 0.3], [0.4, 0.2, 0.1, 0.3]]),
+        ]
+        all_translation_tokens: List[Any] = [None, None]
+        avg_probs, possible_translation_tokens = beam_decode.SequenceGenerator.gather_probs(
+            all_translation_tokens=all_translation_tokens, all_probs=all_probs
+        )
+
+        assert possible_translation_tokens is None
+        avg_probs_ref = [0.65, 0.45, 0.35, 0.55]
+        np.testing.assert_allclose(
+            actual=avg_probs[0], desired=np.array(avg_probs_ref), atol=1e-5
+        )
+        np.testing.assert_allclose(
+            actual=avg_probs[1], desired=np.array(avg_probs_ref), atol=1e-5
+        )
