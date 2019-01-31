3cc6de297ba252f4a029f17617f056acbc474826
Mohammad Sadegh Rasooli <rasooli@fb.com>
2019-01-31 09:35:38 -0800

    segment Viterbi (#318)
    Summary:
    Pull Request resolved: https://github.com/pytorch/translate/pull/318
    
    The goal of this diff is to implement the Viterbi algorithm, a dynamic programming algorithm, for morpheme segmentation. More comments about the detail of this algorithm is in its docstring. You can find some explanation here: http://www.phontron.com/slides/nlp-programming-en-03-ws.pdf
    
    A next diff should provide functionality for actually segmenting words based on the Viterbi algorithm.
    
    Reviewed By: akinh
    
    Differential Revision: D13743633
    
    fbshipit-source-id: 212d70803d67fc7ccd28ee8f30ee5b3ca06e6509

--- pytorch_translate/research/unsupervised_morphology/unsupervised_morphology.py
+++ pytorch_translate/research/unsupervised_morphology/unsupervised_morphology.py
@@ -127,4 +127,89 @@ class MorphologyHMMParams(object):
     def emission_log_probs(self, affix, morpheme):
         if affix in {"START", "END"}:
             return self.SMALL_CONST
+        if self.emission_prob(affix, morpheme) == 0:
+            return self.SMALL_CONST
         return math.log(self.emission_prob(affix, morpheme))
+
+
+class MorphologySegmentor(object):
+    def __init__(self, morphology_hmm_params):
+        self.params = morphology_hmm_params
+
+    def initialize_prob_values(self, n):
+        pi = [{} for _ in range(n + 1)]
+        pi[0]["START"] = 0
+        pi[0]["prefix"] = self.params.SMALL_CONST
+        pi[0]["stem"] = self.params.SMALL_CONST
+        pi[0]["suffix"] = self.params.SMALL_CONST
+        pi[0]["END"] = self.params.SMALL_CONST
+        for i in range(1, n + 1):
+            for affix in self.params.affix_trans_probs.keys():
+                pi[i][affix] = (i + 1) * self.params.SMALL_CONST
+        return pi
+
+    def segment_viterbi(self, word):
+        """
+        This is a dynamic programming algorithm for segmenting a word by using a
+        modified version of the Viterbi algorithm. The main difference here is that
+        the segment-Viterbi algorithm is based on segment-HMM where a state can
+        emit more than one output. In our case, a state is an affix such as prefix,
+        and an output is a substring of a word. Because of searching for a substring
+        the segment-Viterbi algorithm has a slower run-time. In this case (bigram HMM),
+        the runtime complexity of a vanilla Viterbi algorithm is O(nT^2) where n
+        is the number of characters in word and T is the number of possible affixes
+        (in our case 3). The segment-Viterbi has a complexity O(n^2 T^2).
+        A pseudo-code for the vanilla Viterbi algorithm:
+        http://www.cs.columbia.edu/~mcollins/hmms-spring2013.pdf#page=18
+        """
+        n = len(word)
+        pi = self.initialize_prob_values(n)
+        back_pointer = [{} for _ in range(n + 1)]
+
+        for i in range(n):  # loop over starting indices of morphemes
+            for j in range(i + 1, n + 1):  # loop over end indices of morphemes
+                for v in {"prefix", "stem", "suffix"}:  # loop over possible tags
+                    for w in {"START", "prefix", "stem", "suffix"}:
+                        # loop over possible previous tags
+                        t = self.params.transition_log_prob(w, v)
+                        e = self.params.emission_log_probs(v, word[i:j])
+                        log_prob = pi[i][w] + t + e
+                        if log_prob > pi[j][v]:
+                            pi[j][v] = log_prob
+                            # Saving backpointer for previous tag and index.
+                            back_pointer[j][v] = w, i
+
+        # finalizing the best segmentation.
+        best_score = n * self.params.SMALL_CONST
+        best_bp = None
+        indices = [n]  # backtracking indices for segmentation.
+        labels = []  # backtracking labels for segmentation.
+        for v in {"prefix", "stem", "suffix"}:
+            t = self.params.transition_log_prob(v, "END")
+            log_prob = pi[n][v] + t
+            if log_prob > best_score:
+                best_score = log_prob
+                best_bp = back_pointer[n][v][1], v, back_pointer[n][v][0]
+
+        indices.append(best_bp[0])
+        labels.append(best_bp[1])
+        if best_bp[0] > 0:
+            # In cases where the last index is zero, we don't want to backtrack
+            # anymore.
+            labels.append(best_bp[2])
+
+        while True:
+            last_index = indices[-1]
+            last_label = labels[-1]
+            if last_index == 0:
+                break
+            w, i = back_pointer[last_index][last_label]
+            indices.append(i)
+            if i == 0:
+                break
+            labels.append(w)
+
+        # We should now reverse the backtracked list.
+        indices.reverse()
+        labels.reverse()
+        return labels, indices
