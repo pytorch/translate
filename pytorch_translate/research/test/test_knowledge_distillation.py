9286f60cd1d3d49ecf1340f8b8f8bd708ef6e986
Mohammad Sadegh Rasooli <rasooli@fb.com>
2019-03-04 13:27:11 -0800

    offline loading of teacher scores in word-level knowledge distillation (#358)
    Summary:
    Pull Request resolved: https://github.com/pytorch/translate/pull/358
    
    This diff contains structures and function for loading and memoizing teacher model top-k scores in word-level knowledge distillation. We also fixed a normalization bug in the online version KD.
    
    Reviewed By: jhcross
    
    Differential Revision: D14092449
    
    fbshipit-source-id: 5a2dcc5dcb9b479e083aeb5db78147e1f9cfa07e

--- pytorch_translate/research/test/test_knowledge_distillation.py
+++ pytorch_translate/research/test/test_knowledge_distillation.py
@@ -3,8 +3,12 @@
 import unittest
 
 import torch
+import torch.nn.functional as F
 from pytorch_translate import char_source_model  # noqa
 from pytorch_translate import rnn  # noqa
+from pytorch_translate.research.knowledge_distillation import (
+    knowledge_distillation_loss,
+)
 from pytorch_translate.tasks import pytorch_translate_task as tasks
 from pytorch_translate.test import utils as test_utils
 
@@ -20,29 +24,38 @@ class TestKnowledgeDistillation(unittest.TestCase):
         sample = self._dummy_sample()
         model = self.task.build_model(test_args)
         net_output = model(**sample["net_input"])
-        lprobs = model.get_normalized_probs(net_output, log_probs=True)
+        student_probs = model.get_normalized_probs(net_output, log_probs=True)
         # [bsz, seqlen, vocab] -> [bsz*seqlen, vocab]
-        lprobs = lprobs.view(-1, lprobs.size(-1))
+        lprobs = student_probs.view(-1, student_probs.size(-1))
 
         teacher_model = self.task.build_model(test_args)
         teacher_probs = teacher_model.get_normalized_probs(net_output, log_probs=False)
-        teacher_probs = teacher_probs.view(-1, teacher_probs.size(-1)).detach()
+        top_k_teacher_probs, indices = torch.topk(teacher_probs, k=3)
+        top_k_teacher_probs_normalized = F.normalize(
+            top_k_teacher_probs, p=1, dim=2
+        ).detach()
+        sample["top_k_scores"] = top_k_teacher_probs_normalized
+        sample["top_k_indices"] = indices
 
-        # Getting the topk probabilities, masking others, normalizing the topk
-        # probabilities.
-        top_k_probs, indices = torch.topk(teacher_probs, k=3)
-        top_k_probs_normalized = top_k_probs / torch.sum(top_k_probs)
-        topk_mask = torch.zeros(teacher_probs.shape)
-        topk_probs = topk_mask.scatter(1, indices, top_k_probs_normalized)
+        kd_criterion = knowledge_distillation_loss.KnowledgeDistillationCriterion(
+            test_args, self.task
+        )
+        kd_loss, topk_probs = kd_criterion.get_kd_loss(sample, student_probs, lprobs)
 
-        # asserting that the values are correctly asserted into teacher_probs.
-        for i, index in enumerate(indices):
-            print(index, i)
-            for j in index:
-                if teacher_probs[i][j] > 0:
-                    assert topk_probs[i][j] > 0
+        # asserting that the values are correctly inserted into teacher_probs.
+        for row in range(indices.shape[0]):
+            for col in range(indices.shape[1]):
+                # testing if values are normalized.
+                assert round(float(torch.sum(topk_probs[row][col][:])), 0) == 1.0
+                for i, val in enumerate(indices[row][col]):
+                    # testing if scattering is done correctly.
+                    assert (
+                        topk_probs[row][col][val]
+                        == top_k_teacher_probs_normalized[row][col][i]
+                    )
 
-        kd_loss = -torch.sum(topk_probs * lprobs)
+        topk_probs_flat = topk_probs.view(-1, topk_probs.size(-1))
+        kd_loss = -torch.sum(topk_probs_flat * lprobs)
         assert kd_loss >= 0
 
     def _dummy_sample(self):
