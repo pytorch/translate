9d14ef5fdf9591b26b8d0addc274aebae12d0fa3
Halil Akin <halilakin@fb.com>
2019-04-03 15:19:51 -0700

    Allow reverse rescoring to work on any model [8/n]
    Summary: Reverse model rescoring currently expects a model with a dictionary identical to the original model. In this diff, I am removing this limitation by converting token ids to string and reverting back.
    
    Differential Revision: D14761210
    
    fbshipit-source-id: 535dd6780e209e20395fa730496c85b60da81b9f

--- pytorch_translate/rescoring/test/test_model_scorers.py
+++ pytorch_translate/rescoring/test/test_model_scorers.py
@@ -4,7 +4,10 @@ import unittest
 from unittest.mock import patch
 
 import torch
-from pytorch_translate.rescoring.model_scorers import SimpleModelScorer
+from pytorch_translate.rescoring.model_scorers import (
+    ReverseModelScorer,
+    SimpleModelScorer,
+)
 from pytorch_translate.tasks import pytorch_translate_task as tasks
 from pytorch_translate.test import utils as test_utils
 
@@ -61,3 +64,34 @@ class TestModelScorers(unittest.TestCase):
                 ]
             ).type_as(tgt_tokens)
             assert torch.equal(tgt_tokens, expected_tgt_tokens)
+
+    def test_reverse_scorer_prepare_inputs(self):
+        test_args = test_utils.ModelParamsDict()
+        _, src_dict, tgt_dict = test_utils.prepare_inputs(test_args)
+        task = tasks.PytorchTranslateTask(test_args, src_dict, tgt_dict)
+        model = task.build_model(test_args)
+
+        with patch(
+            "pytorch_translate.utils.load_diverse_ensemble_for_inference",
+            return_value=([model], test_args, task),
+        ):
+            scorer = ReverseModelScorer(test_args, None, task)
+
+            src_tokens = torch.tensor([6, 7, 8], dtype=torch.int)
+            hypo = {"tokens": torch.tensor([12, 13, 14], dtype=torch.int)}
+            encoder_inputs, tgt_tokens = scorer.prepare_inputs(src_tokens, hypo)
+
+            # make sure encoder input is equal to hypo target
+            assert torch.equal(encoder_inputs[0][0], hypo["tokens"])
+            # make sure new target is equal to source tokens + eos tokens
+            assert torch.equal(
+                torch.cat(
+                    (
+                        torch.tensor([2], dtype=torch.int),
+                        src_tokens,
+                        torch.tensor([2], dtype=torch.int),
+                    ),
+                    dim=0,
+                ).unsqueeze(dim=0),
+                tgt_tokens,
+            )
