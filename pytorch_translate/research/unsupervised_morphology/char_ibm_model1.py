d12dfb4484c1fc659bdd31ee784a01aa5563bfc5
Mohammad Sadegh Rasooli <rasooli@fb.com>
2019-04-15 11:16:42 -0700

    Bilingual BPE --> part 16 (character ibm_model1 -- part 2) (#450)
    Summary:
    Pull Request resolved: https://github.com/pytorch/translate/pull/450
    
    Adding subword extractor that is to be used in different places for the subword-based IBM model.
    
    Reviewed By: dpacgopinath
    
    Differential Revision: D14884485
    
    fbshipit-source-id: ebc9c4ee4045fc01d0cf739c1badf9b4282499a3

--- pytorch_translate/research/unsupervised_morphology/char_ibm_model1.py
+++ pytorch_translate/research/unsupervised_morphology/char_ibm_model1.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
 
+from collections import Counter
+from typing import Dict
+
 from pytorch_translate.research.unsupervised_morphology import ibm_model1
 
 
 class CharIBMModel1(ibm_model1.IBMModel1):
-    def __init__(self):
+    def __init__(self, max_subword_len: int = 8):
         super().__init__()
         self.eow_symbol = "_EOW"  # End of word symbol.
+        self.max_subword_len = max_subword_len
+
+    def get_possible_subwords(self, word: str) -> Dict[str, int]:
+        subwords = Counter()
+        chars = list(word) + [self.eow_symbol]
+        for i in range(len(chars)):
+            for j in range(i + 1, min(i + 1 + self.max_subword_len, len(chars) + 1)):
+                subword = "".join(chars[i:j])
+                subwords[subword] += 1
+        return subwords
