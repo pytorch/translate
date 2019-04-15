36bf005e8ef80ae63cbe90013c7a104399d7ded3
Halil Akin <halilakin@fb.com>
2019-04-15 15:06:08 -0700

    Fix failing test_scorer test (#464)
    Summary:
    Pull Request resolved: https://github.com/pytorch/translate/pull/464
    
    https://our.intern.facebook.com/intern/test/281474988393180?ref_report_id=0  has been failing after latest changes. The reason is because our test class didn't have the same params. And also we changed the logic without updating corresponding test.
    
    Reviewed By: rasoolims
    
    Differential Revision: D14941067
    
    fbshipit-source-id: 998f71c92cd17d2740bb4cdcc8402cad7a286165

--- pytorch_translate/rescoring/test/test_rescorer.py
+++ pytorch_translate/rescoring/test/test_rescorer.py
@@ -12,6 +12,7 @@ class TestRescorer(unittest.TestCase):
     def test_combine_weighted_scores(self):
         test_args = test_utils.ModelParamsDict()
         test_args.enable_rescoring = True
+        test_args.length_penalty = 1
         test_args.original_model_weight = 1
         test_args.r2l_model_weight = 0
         test_args.reverse_model_weight = 0.5
@@ -26,6 +27,6 @@ class TestRescorer(unittest.TestCase):
         hypos = [{"tokens": torch.tensor([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])}]
         rescorer.combine_weighted_scores(scores, src_tokens, hypos)
 
-        # 10/10=1. , 20*0=0. , 30*(0.5/5)=3. , 40*(0.75/5)=6.
-        expected = torch.tensor([[1.0, 0.0, 3.0, 6.0]], dtype=torch.float)
+        # 10/1=10. , 20*0=0. , 30*(0.5/5)=3. , 40*(0.75/5)=6.
+        expected = torch.tensor([[10.0, 0.0, 3.0, 6.0]], dtype=torch.float)
         assert torch.equal(scores, expected)
