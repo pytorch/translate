652f009873030ae619c06f235066e03278463c30
Mohammad Sadegh Rasooli <rasooli@fb.com>
2019-04-17 16:40:45 -0700

    Bilingual BPE --> part 21 ( bilingual part 1)
    Differential Revision: D14941604
    
    fbshipit-source-id: b488ccd3ca0561cc5cb4de6e508de9f36a947410

--- pytorch_translate/research/test/morphology_test_utils.py
+++ pytorch_translate/research/test/morphology_test_utils.py
@@ -4,7 +4,7 @@ import tempfile
 from os import path
 
 
-def get_two_tmp_files():
+def get_two_same_tmp_files():
     src_txt_content = [
         "123 124 234 345",
         "112 122 123 345",
@@ -16,6 +16,30 @@ def get_two_tmp_files():
         "112 122 123 345",
         "123456789",
         "123456 456789",
+    ]
+    content1, content2 = "\n".join(src_txt_content), "\n".join(dst_txt_content)
+    tmp_dir = tempfile.mkdtemp()
+    file1, file2 = path.join(tmp_dir, "test1.txt"), path.join(tmp_dir, "test2.txt")
+    with open(file1, "w") as f1:
+        f1.write(content1)
+    with open(file2, "w") as f2:
+        f2.write(content2)
+
+    return tmp_dir, file1, file2
+
+
+def get_two_different_tmp_files():
+    src_txt_content = [
+        "123 124 234 345",
+        "112 122 123 345",
+        "123456789",
+        "123456 456789",
+    ]
+    dst_txt_content = [
+        "abc abd bcd cde",
+        "aab abb abc cde",
+        "abcdefghi",
+        "abcdef defghi",
     ]
     content1, content2 = "\n".join(src_txt_content), "\n".join(dst_txt_content)
     tmp_dir = tempfile.mkdtemp()
--- pytorch_translate/research/test/test_char_ibm_model.py
+++ pytorch_translate/research/test/test_char_ibm_model.py
@@ -34,7 +34,7 @@ class TestCharIBMModel1(unittest.TestCase):
     def test_morph_init(self):
         ibm_model = CharIBMModel1()
 
-        tmp_dir, f1, f2 = morph_utils.get_two_tmp_files()
+        tmp_dir, f1, f2 = morph_utils.get_two_same_tmp_files()
         ibm_model.initialize_translation_probs(f1, f2)
         assert ibm_model.translation_prob["5"]["5" + ibm_model.eow_symbol] > 0
         assert len(ibm_model.translation_prob) == 80
@@ -44,7 +44,7 @@ class TestCharIBMModel1(unittest.TestCase):
     def test_em_step(self):
         ibm_model = CharIBMModel1()
 
-        tmp_dir, f1, f2 = morph_utils.get_two_tmp_files()
+        tmp_dir, f1, f2 = morph_utils.get_two_same_tmp_files()
         ibm_model.initialize_translation_probs(src_path=f1, dst_path=f2)
 
         pool = Pool(3)
