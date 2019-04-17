2454d22f85e1c7da1e4592cda2b4877c1aac033a
Mohammad Sadegh Rasooli <rasooli@fb.com>
2019-04-17 12:04:01 -0700

    Bilingual BPE --> part 17 (character ibm_model1 -- part 3) (#451)
    Summary:
    Pull Request resolved: https://github.com/pytorch/translate/pull/451
    
    In this diff I wrote the initializer for the subword-based IBM model. In order to decrease code redundancy I create a morphology utils file that is used both in the original IBM model as well the subword-based IBM model.
    
    Reviewed By: dpacgopinath
    
    Differential Revision: D14885790
    
    fbshipit-source-id: e44463e51d316f44809f508eda4123f499cfeadc

---
+++ pytorch_translate/research/test/morphology_test_utils.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import tempfile
+from os import path
+
+
+def get_two_tmp_files():
+    src_txt_content = [
+        "123 124 234 345",
+        "112 122 123 345",
+        "123456789",
+        "123456 456789",
+    ]
+    dst_txt_content = [
+        "123 124 234 345",
+        "112 122 123 345",
+        "123456789",
+        "123456 456789",
+    ]
+    content1, content2 = "\n".join(src_txt_content), "\n".join(dst_txt_content)
+    tmp_dir = tempfile.mkdtemp()
+    file1, file2 = path.join(tmp_dir, "test1.txt"), path.join(tmp_dir, "test2.txt")
+    with open(file1, "w") as f1:
+        f1.write(content1)
+    with open(file2, "w") as f2:
+        f2.write(content2)
+
+    return tmp_dir, file1, file2
