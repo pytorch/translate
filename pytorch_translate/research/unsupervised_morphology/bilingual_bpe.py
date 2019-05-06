90d9eb3dac0d81e0ea55ff7b5a0755f9b7466e86
Mohammad Sadegh Rasooli <rasooli@fb.com>
2019-05-06 14:56:49 -0700

    Saving and loading each step separately (#504)
    Summary:
    Pull Request resolved: https://github.com/pytorch/translate/pull/504
    
    Saving/loading models instead of running the whole pipeline in one shot (good for reusability)
    
    Differential Revision: D15124109
    
    fbshipit-source-id: a2d0015f8ce08f45ee9a0bbe1bc05c2c6925d7f0

--- pytorch_translate/research/unsupervised_morphology/bilingual_bpe.py
+++ pytorch_translate/research/unsupervised_morphology/bilingual_bpe.py
@@ -52,6 +52,7 @@ def get_arg_parser():
         help="Number of training epochs for character IBM models.",
         default=3,
     )
+    parser.add_option("--model", type="str", dest="model_path", help="Model Path.")
     return parser
 
 
@@ -60,27 +61,16 @@ class BilingualBPE(BPE):
     An extension of the BPE model that is cross-lingual wrt parallel data.
     """
 
-    def __init__(self):
-        super().__init__()
-        self.dst2src_ibm_model = Word2CharIBMModel1()
-
-    def _init_params(self, src_txt_path: str, dst_txt_path: str, num_ibm_iters: int):
+    def _init_params(self, ibm_model_path: str, src_txt_path: str, dst_txt_path: str):
         """
         Args:
             src_txt_path: Text path for source language in parallel data.
             dst_txt_path: Text path for target language in parallel data.
             num_ibm_iters: Number of training epochs for the IBM model.
         """
-        logger.info("Initializing vocabulary.")
-
-        # Note the reverse side of the model. Target is word based, that is why
-        # we give it a reverse order.
-        self.dst2src_ibm_model.learn_ibm_parameters(
-            src_path=dst_txt_path, dst_path=src_txt_path, num_iters=num_ibm_iters
-        )
         logger.info("calculating alignment-based BPE type probs.")
         self.bpe_probs_from_alignment = self._calc_bpe_prob_from_alignment(
-            dst_txt_path=dst_txt_path
+            ibm_model_path=ibm_model_path, dst_txt_path=dst_txt_path
         )
 
         # Need to call this at the end, because this funciton calls the
@@ -102,20 +92,25 @@ class BilingualBPE(BPE):
             vocab[word] /= denom
         return vocab
 
-    def _calc_bpe_prob_from_alignment(self, dst_txt_path: str) -> Dict[str, float]:
+    def _calc_bpe_prob_from_alignment(
+        self, ibm_model_path: str, dst_txt_path: str
+    ) -> Dict[str, float]:
         """
          p(subword=s) = sum_{t in target} p(s|t) p(t)
          where p(t) is target_word_prob[t] from _calc_word_probs
          and p(s|t) = self.dst2src_ibm_model.translation_prob[t][s]
         """
+
+        dst2src_ibm_model = Word2CharIBMModel1()
+        dst2src_ibm_model.load(file_path=ibm_model_path)
         target_word_probs = self._calc_word_probs(txt_path=dst_txt_path)
         bpe_alignment_prob = defaultdict(float)
-        for dst_word_id in list(self.dst2src_ibm_model.translation_prob.keys()):
-            dst_word = self.dst2src_ibm_model.int2str(dst_word_id)
+        for dst_word_id in list(dst2src_ibm_model.translation_prob.keys()):
+            dst_word = dst2src_ibm_model.int2str(dst_word_id)
             target_word_prob = target_word_probs[dst_word]
-            alignment_probs = self.dst2src_ibm_model.translation_prob[dst_word_id]
+            alignment_probs = dst2src_ibm_model.translation_prob[dst_word_id]
             for src_subword_id in list(alignment_probs.keys()):
-                src_subword = self.dst2src_ibm_model.int2str(src_subword_id)
+                src_subword = dst2src_ibm_model.int2str(src_subword_id)
                 bpe_alignment_prob[src_subword] += (
                     alignment_probs[src_subword] * target_word_prob
                 )
@@ -190,16 +185,16 @@ class BilingualBPE(BPE):
             del self.vocab[old_tokens[-1]]
 
     def build_vocab(
-        self, src_txt_path: str, dst_txt_path: str, vocab_size: int, num_ibm_iters: int
+        self, ibm_model_path: str, src_txt_path: str, dst_txt_path: str, vocab_size: int
     ):
         """
         Note that except initalization, other parts are the same as the
         original bpe build_vocab method.
         """
         self._init_params(
+            ibm_model_path=ibm_model_path,
             src_txt_path=src_txt_path,
             dst_txt_path=dst_txt_path,
-            num_ibm_iters=num_ibm_iters,
         )
         return self._build_vocab_loop(vocab_size=vocab_size)
 
@@ -207,13 +202,25 @@ class BilingualBPE(BPE):
 if __name__ == "__main__":
     arg_parser = get_arg_parser()
     options, args = arg_parser.parse_args()
+
+    # Note the reverse side of the model. Target is word based, that is why
+    # we give it a reverse order.
+    dst2src_ibm_model = Word2CharIBMModel1()
+    dst2src_ibm_model.learn_ibm_parameters(
+        src_path=options.dst_train_file,
+        dst_path=options.src_train_file,
+        num_iters=options.num_ibm_iters,
+    )
+    dst2src_ibm_model.save(file_path=options.model_path + ".ibm")
+
     bpe_model = BilingualBPE()
     bpe_model.build_vocab(
+        ibm_model_path=options.model_path + ".ibm",
         src_txt_path=options.src_train_file,
         dst_txt_path=options.dst_train_file,
         vocab_size=options.vocab_size,
-        num_ibm_iters=options.num_ibm_iters,
     )
     bpe_model.segment_txt(
         input_path=options.src_train_file, output_path=options.train_output_file
     )
+    bpe_model.save(file_path=options.model_path)
