e7c0bb3719ef9f2298b45bd1c4d41bcc2c37515b
Arun Babu <arbabu@fb.com>
2019-05-06 15:54:04 -0700

    Add torchscript version of Beam decode (#402)
    Summary:
    Pull Request resolved: https://github.com/pytorch/translate/pull/402
    
    Add torchscript version of Beam Decode so that it can be exported as the part of model.
    
    Reviewed By: jmp84
    
    Differential Revision: D14525888
    
    fbshipit-source-id: b697c5c89e925f97704c83d5bf21278c0874cee7

--- pytorch_translate/beam_decode.py
+++ pytorch_translate/beam_decode.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
 import math
+from typing import List, Tuple
 
 import torch
 from fairseq import search, utils
 from fairseq.models import FairseqIncrementalDecoder
 from pytorch_translate import utils as pytorch_translate_utils
+from torch import Tensor
 
 
 class SequenceGenerator(object):
@@ -740,3 +742,221 @@ class SequenceGenerator(object):
         ] = diversity_sibling_rank
         logprobs -= gamma * diverse_penalty
         return logprobs
+
+
+class BeamDecode(torch.jit.ScriptModule):
+    """
+    Decodes the output of Beam Search to get the top hypotheses
+    """
+
+    def __init__(self, eos_token_id, length_penalty, nbest, beam_size, stop_at_eos):
+        super().__init__()
+        self.eos_token_id = torch.jit.Attribute(eos_token_id, int)
+        self.length_penalty = torch.jit.Attribute(length_penalty, float)
+        self.nbest = torch.jit.Attribute(nbest, int)
+        self.beam_size = torch.jit.Attribute(beam_size, int)
+        self.stop_at_eos = torch.jit.Attribute(int(stop_at_eos), int)
+
+    @torch.jit.script_method
+    @torch.no_grad()
+    def forward(
+        self,
+        beam_tokens: Tensor,
+        beam_scores: Tensor,
+        token_weights: Tensor,
+        beam_prev_indices: Tensor,
+        num_steps: int,
+    ) -> List[Tuple[Tensor, float, List[float], List[Tensor]]]:
+
+        self._check_dimensions(
+            beam_tokens, beam_scores, token_weights, beam_prev_indices, num_steps
+        )
+
+        end_states = self._get_all_end_states(
+            beam_tokens, beam_scores, beam_prev_indices, num_steps
+        )
+
+        # outputs is
+        # Tuple[Hypothesis, Hypothesis score, Token level scores, Attention Weights]
+        outputs = torch.jit.annotate(
+            List[Tuple[Tensor, float, List[float], List[Tensor]]], []
+        )
+
+        for state_idx in range(len(end_states)):
+            state = end_states[state_idx]
+            hypothesis_score = float(state[0])
+            beam_indices = self._get_output_steps_to_beam_indices(
+                state, beam_prev_indices
+            )
+            beam_output = torch.jit.annotate(List[Tensor], [])
+            token_level_scores = torch.jit.annotate(List[float], [])
+            position = int(state[1])
+            back_alignment_weights = []
+
+            assert position + 1 == len(beam_indices)
+            pos = 1
+            prev_beam_index = -1
+            while pos < len(beam_indices):
+                beam_index = beam_indices[pos]
+                beam_output.append(beam_tokens[pos][beam_index])
+                if pos == 1:
+                    # beam_scores[0][:] are all 0s
+                    token_level_scores.append(float(beam_scores[pos][beam_index]))
+                else:
+                    token_level_scores.append(
+                        float(beam_scores[pos][beam_index])
+                        - float(beam_scores[pos - 1][prev_beam_index])
+                    )
+                back_alignment_weights.append(token_weights[pos][beam_index].detach())
+                prev_beam_index = beam_index
+                pos += 1
+            outputs.append(
+                (
+                    torch.stack(beam_output),
+                    hypothesis_score,
+                    token_level_scores,
+                    back_alignment_weights,
+                )
+            )
+
+        return outputs
+
+    @torch.jit.script_method
+    def _get_output_steps_to_beam_indices(
+        self, end_state: Tensor, beam_prev_indices: Tensor
+    ) -> List[int]:
+        """
+        Returns a mapping from each output position and the beam index that was
+        picked from the beam search results.
+        """
+        present_position = int(end_state[1])
+        beam_index = int(end_state[2])
+        beam_indices = torch.jit.annotate(List[int], [])
+        while present_position >= 0:
+            beam_indices.insert(0, beam_index)
+            beam_index = int(beam_prev_indices[present_position][beam_index])
+            present_position = present_position - 1
+        return beam_indices
+
+    @torch.jit.script_method
+    def _add_to_end_states(
+        self, end_states: List[Tensor], min_score: float, state: Tensor
+    ) -> Tuple[List[Tensor], float]:
+        """
+        Maintains a list of atmost `nbest` highest end states
+        """
+        if len(end_states) < self.nbest:
+            end_states.append(state)
+        elif bool(state[0] > min_score):
+            min_index = -1
+            min_score = float("inf")
+            for idx in range(len(end_states)):
+                s = end_states[idx]
+                if bool(float(s[0]) < min_score):
+                    min_index = idx
+                    min_score = float(s[0])
+            end_states[min_index] = state
+        return end_states, min_score
+
+    @torch.jit.script_method
+    def _get_all_end_states(
+        self,
+        beam_tokens: Tensor,
+        beam_scores: Tensor,
+        beam_prev_indices: Tensor,
+        num_steps: int,
+    ) -> Tensor:
+        """
+        Return all end states and hypothesis scores for those end states.
+        """
+        min_score = -float("inf")
+        end_states = torch.jit.annotate(List[Tensor], [])
+        prev_hypo_is_finished = torch.zeros(self.beam_size).byte()
+
+        position = 1
+        while bool(position <= num_steps):
+            hypo_is_finished = torch.zeros(self.beam_size).byte()
+
+            for hyp_index in range(self.beam_size):
+                prev_pos = beam_prev_indices[position][hyp_index]
+                hypo_is_finished[hyp_index] = prev_hypo_is_finished[prev_pos]
+
+                # If hypothesis was completed in the previous index,
+                # then just continue
+                if bool(hypo_is_finished[hyp_index] == 0):
+                    # If the present token is EOS or we have reached max_length
+                    # then hypothesis is complete
+                    if bool(
+                        beam_tokens[position][hyp_index] == self.eos_token_id
+                    ) or bool(position == num_steps):
+
+                        if bool(self.stop_at_eos):
+                            hypo_is_finished[hyp_index] = 1
+
+                        hypo_score = float(beam_scores[position][hyp_index])
+                        if bool(self.length_penalty != 0):
+                            hypo_score = hypo_score / float(position) ** float(
+                                self.length_penalty
+                            )
+
+                        end_states, min_score = self._add_to_end_states(
+                            end_states,
+                            min_score,
+                            torch.tensor(
+                                [hypo_score, float(position), float(hyp_index)]
+                            ),
+                        )
+
+            prev_hypo_is_finished = hypo_is_finished
+            position = position + 1
+
+        end_states = torch.stack(end_states)
+
+        _, sorted_end_state_indices = end_states[:, 0].sort(dim=0, descending=True)
+        end_states = end_states[sorted_end_state_indices, :]
+        return end_states
+
+    @torch.jit.script_method
+    def _check_dimensions(
+        self,
+        beam_tokens: Tensor,
+        beam_scores: Tensor,
+        token_weights: Tensor,
+        beam_prev_indices: Tensor,
+        num_steps: int,
+    ) -> None:
+
+        assert (
+            beam_tokens.size(1) == self.beam_size
+        ), "Dimension of beam_tokens : {} and beam size : {} are not consistent".format(
+            beam_tokens.size(), self.beam_size
+        )
+        assert beam_scores.size(1) == self.beam_size, (
+            "Dimension of beam_scores : {} and beam size : {} "
+            "are not consistent".format(beam_scores.size(), self.beam_size)
+        )
+        assert token_weights.size(1) == self.beam_size, (
+            "Dimension of token_weights : {} and beam size : {} "
+            "are not consistent".format(token_weights.size(), self.beam_size)
+        )
+        assert (
+            beam_prev_indices.size(1) == self.beam_size
+        ), "Dimension of beam_prev_indices : {} and beam size : {} "
+        "are not consistent".format(beam_prev_indices.size(), self.beam_size)
+
+        assert beam_tokens.size(0) <= num_steps + 1, (
+            "Dimension of beam_tokens : {} and num_steps : {} "
+            "are not consistent".format(beam_tokens.size(), num_steps)
+        )
+        assert beam_scores.size(0) <= num_steps + 1, (
+            "Dimension of beam_scores : {} and num_steps : {} "
+            "are not consistent".format(beam_scores.size(), num_steps)
+        )
+        assert token_weights.size(0) <= num_steps + 1, (
+            "Dimension of token_weights : {} and num_steps : {} "
+            "are not consistent".format(token_weights.size(), num_steps)
+        )
+        assert beam_prev_indices.size(0) <= num_steps + 1, (
+            "Dimension of beam_prev_indices : {} and num_steps : {} "
+            "are not consistent".format(beam_prev_indices.size(), num_steps)
+        )
