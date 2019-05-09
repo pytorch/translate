abc324c84a9875e9d63ac939019c2628eb0bf374
Mohammad Sadegh Rasooli <rasooli@fb.com>
2019-05-09 16:05:23 -0700

    Making IBM model non-parallel due to wierd IPC problem when dealing with big text (#502)
    Summary:
    Pull Request resolved: https://github.com/pytorch/translate/pull/502
    
    Making the ibm model single-process due to weird problems in multi-processing.
    
    Reviewed By: qingerVT
    
    Differential Revision: D15068796
    
    fbshipit-source-id: de8269ed51aa06770db60925539be07ac2da2c66

--- pytorch_translate/research/test/test_ibm_model.py
+++ pytorch_translate/research/test/test_ibm_model.py
@@ -4,7 +4,6 @@ import shutil
 import tempfile
 import unittest
 from collections import Counter, defaultdict
-from multiprocessing import Pool
 from os import path
 
 from pytorch_translate.research.test import morphology_test_utils as morph_utils
@@ -23,14 +22,14 @@ class TestIBMModel1(unittest.TestCase):
         assert ibm_model.translation_prob["122"]["123"] == 1.0 / 4
         shutil.rmtree(tmp_dir)
 
-    def test_e_step(self):
+    def test_expectation_for_one_sentence(self):
         ibm_model = IBMModel1()
 
         tmp_dir, f1, f2 = morph_utils.get_two_same_tmp_files()
         ibm_model.initialize_translation_probs(f1, f2)
         translation_counts = defaultdict(lambda: defaultdict(float))
 
-        ibm_model.e_step(
+        ibm_model.expectation_for_one_sentence(
             Counter(["123", "124", "234", "345", ibm_model.null_str]),
             Counter(["123", "124", "234", "345"]),
             translation_counts,
@@ -38,26 +37,11 @@ class TestIBMModel1(unittest.TestCase):
         assert round(translation_counts["123"]["345"], 3) == 0.176
         shutil.rmtree(tmp_dir)
 
-    def test_em_step(self):
-        ibm_model = IBMModel1()
-
-        tmp_dir, f1, f2 = morph_utils.get_two_same_tmp_files()
-        ibm_model.initialize_translation_probs(f1, f2)
-
-        pool = Pool(3)
-        ibm_model.em_step(src_path=f1, dst_path=f2, num_cpus=3, pool=pool)
-
-        assert ibm_model.translation_prob["456789"]["345"] == 0
-        assert ibm_model.translation_prob["456789"]["456789"] == 0.5
-        shutil.rmtree(tmp_dir)
-
     def test_ibm_train(self):
         ibm_model = IBMModel1()
 
         tmp_dir, f1, f2 = morph_utils.get_two_same_tmp_files()
-        ibm_model.learn_ibm_parameters(
-            src_path=f1, dst_path=f2, num_iters=3, num_cpus=3
-        )
+        ibm_model.learn_ibm_parameters(src_path=f1, dst_path=f2, num_iters=3)
 
         assert ibm_model.translation_prob["456789"]["345"] == 0
         assert ibm_model.translation_prob["456789"]["456789"] == 0.5
