bc177658f249ae4eed34ffdaca8fce72a966c433
Halil Akin <halilakin@fb.com>
2019-05-20 16:13:05 -0700

    Refactor prepare_inputs and add tests (#542)
    Summary:
    Pull Request resolved: https://github.com/pytorch/translate/pull/542
    
    Multiple things
    
    * pytorch_translate rnn models expect encoder_inputs tuple's src_lengths to be the same size as src_tokens.
    * remove prepare_encoder_inputs
    * add tests for prepare_inputs
    
    Differential Revision: D15423761
    
    fbshipit-source-id: 405b43d917d1c3342babd91b5301ea0529f3c50c

--- pytorch_translate/rescoring/test/test_model_scorers.py
+++ pytorch_translate/rescoring/test/test_model_scorers.py
@@ -83,6 +83,42 @@ class TestModelScorers(unittest.TestCase):
             assert hypos_scores[0] == 2.0
             assert hypos_scores[1] == 4.5
 
+    def test_simple_scorer_prepare_inputs(self):
+        self.args.append_eos_to_source = True
+        pad = self.task.tgt_dict.pad()
+        eos = self.task.tgt_dict.eos()
+
+        src_tokens = torch.tensor([6, 7, 8], dtype=torch.int)
+        hypos = [
+            {"tokens": torch.tensor([12, 13, 14, eos], dtype=torch.int)},
+            {"tokens": torch.tensor([22, 23, eos], dtype=torch.int)},
+        ]
+
+        with patch(
+            "pytorch_translate.utils.load_diverse_ensemble_for_inference",
+            return_value=([self.model], self.args, self.task),
+        ):
+            scorer = SimpleModelScorer(
+                self.args, "/tmp/model_path.txt", None, self.task
+            )
+            (encoder_inputs, tgt_tokens) = scorer.prepare_inputs(src_tokens, hypos)
+
+            # Test encoder inputs
+            assert torch.equal(
+                encoder_inputs[0], torch.tensor([[6, 7, 8], [6, 7, 8]], dtype=torch.int)
+            ), "Encoder inputs are not as expected"
+            assert torch.equal(
+                encoder_inputs[1], torch.tensor([3, 3], dtype=torch.int)
+            ), "Src lengths are not as expected"
+
+            # Test target tokens
+            assert torch.equal(
+                tgt_tokens,
+                torch.tensor(
+                    [[eos, 12, 13, 14, eos], [eos, 22, 23, eos, pad]], dtype=torch.int
+                ),
+            ), "Target tokens are not as expected"
+
     def test_reverse_scorer_prepare_inputs(self):
         self.args.append_eos_to_source = True
         pad = self.task.tgt_dict.pad()
