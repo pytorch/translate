172d21674b5d0a5d07d46f29ebaab2914fc3dadd
Liz Guo <lizguo@fb.com>
2019-07-28 22:40:18 -0700

    fix bug in beamDecode (#605)
    Summary:
    Pull Request resolved: https://github.com/pytorch/translate/pull/605
    
    In beam decoding, keep min_score updated when adding new state to the list of nBest end_states. Otherwise the last element in the list might be replaced by mistake.
    
    Reviewed By: akinh, arbabu123, jhcross
    
    Differential Revision: D16495377
    
    fbshipit-source-id: dad697e9e6f97c0ee227c2881a853accec0b173f

--- pytorch_translate/test/test_beam_search_and_decode.py
+++ pytorch_translate/test/test_beam_search_and_decode.py
@@ -5,7 +5,7 @@ import unittest
 import numpy as np
 import torch
 from pytorch_translate import rnn  # noqa
-from pytorch_translate.beam_decode import SequenceGenerator
+from pytorch_translate.beam_decode import BeamDecode, SequenceGenerator
 from pytorch_translate.ensemble_export import BeamSearchAndDecode
 from pytorch_translate.tasks import pytorch_translate_task as tasks
 from pytorch_translate.test import utils as test_utils
@@ -130,3 +130,74 @@ class TestBeamSearchAndDecode(unittest.TestCase):
             )
             # Not testing the hypothesis score as sequence generator is adding EOS
             # at the end, it changes the final score
+
+    def test_get_all_end_states(self):
+        """
+        test the function of keeping nBest end_states.
+        In this example, all end_states should be: ([-4.6246, 1, 1](hit eos),
+        [-7.0106, 2, 2](hit eos), [-8.5362, 3, 0](hit maxLen),
+        [-9.4792, 3, 1](hit maxLen), [-10.4673, 3, 2](hit maxLen)),
+        nBest should be the first three.
+        """
+        beam_tokens = torch.tensor([[2, 2, 2], [3, 2, 4], [1, 5, 2], [6, 6, 3]])
+        beam_scores = torch.tensor(
+            [
+                [0.0000, 0.0000, 0.0000],
+                [-3.6237, -4.6246, -4.6427],
+                [-6.5691, -6.9823, -7.0106],
+                [-8.5362, -9.4792, -10.4673],
+            ]
+        )
+        beam_prev_indices = torch.tensor([[0, 0, 0], [0, 0, 0], [0, 0, 2], [1, 0, 1]])
+
+        beam_decode = BeamDecode(
+            eos_token_id=2, length_penalty=0.0, nbest=3, beam_size=3, stop_at_eos=True
+        )
+
+        all_end_states = beam_decode._get_all_end_states(
+            beam_tokens, beam_scores, beam_prev_indices, num_steps=3
+        )
+        desired_end_states_1 = torch.tensor([-4.6246, 1, 1])
+        desired_end_states_2 = torch.tensor([-7.0106, 2, 2])
+        desired_end_states_3 = torch.tensor([-8.5362, 3, 0])
+        np.testing.assert_array_equal(
+            all_end_states[0, :].numpy(), desired_end_states_1.numpy()
+        )
+        np.testing.assert_array_equal(
+            all_end_states[1, :].numpy(), desired_end_states_2.numpy()
+        )
+        np.testing.assert_array_equal(
+            all_end_states[2, :].numpy(), desired_end_states_3.numpy()
+        )
+
+    def test_get_all_end_states_with_same_scores(self):
+        """
+        test the function of keeping nBest end_states
+        When two hypotheses have same scores, keep the first one.
+        """
+        beam_tokens = torch.tensor([[2, 2, 2, 2], [3, 4, 5, 6]])
+        beam_scores = torch.tensor(
+            [[0.0000, 0.0000, 0.0000, 0.0000], [-4.4000, -4.4000, -4.2000, -4.3000]]
+        )
+        beam_prev_indices = torch.tensor([[0, 0, 0, 0], [0, 0, 0, 0]])
+        beam_decode = BeamDecode(
+            eos_token_id=2, length_penalty=0.0, nbest=3, beam_size=4, stop_at_eos=True
+        )
+
+        all_end_states = beam_decode._get_all_end_states(
+            beam_tokens, beam_scores, beam_prev_indices, num_steps=1
+        )
+
+        desired_end_states_1 = torch.tensor([-4.2000, 1, 2])
+        desired_end_states_2 = torch.tensor([-4.3000, 1, 3])
+        # keep the first hypo [-4.4000, 1, 0] rather than [-4.4000, 1, 1]
+        desired_end_states_3 = torch.tensor([-4.4000, 1, 0])
+        np.testing.assert_array_equal(
+            all_end_states[0, :].numpy(), desired_end_states_1.numpy()
+        )
+        np.testing.assert_array_equal(
+            all_end_states[1, :].numpy(), desired_end_states_2.numpy()
+        )
+        np.testing.assert_array_equal(
+            all_end_states[2, :].numpy(), desired_end_states_3.numpy()
+        )
