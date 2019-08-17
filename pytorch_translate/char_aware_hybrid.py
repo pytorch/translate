95c7f5034c4449bcff794cc56f2e7bd7e5a93613
Mohammad Sadegh Rasooli <rasooli@fb.com>
2019-08-16 22:01:07 -0700

    Character Aware Decoder (Part 2) (#625)
    Summary:
    Pull Request resolved: https://github.com/pytorch/translate/pull/625
    
    In this diff, I added the forward function for the char-aware model. A next diff should override the eval model, such the the character representations for each word is added to the word embeddings, thus there won't be any need to use the characters in inference.
    
    Reviewed By: jhcross
    
    Differential Revision: D16788701
    
    fbshipit-source-id: 63799e45d1a7af0305db46fecc8cd05d5e4b025b

--- pytorch_translate/char_aware_hybrid.py
+++ pytorch_translate/char_aware_hybrid.py
@@ -2,7 +2,9 @@
 
 from ast import literal_eval
 
+import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from pytorch_translate import char_encoder, hybrid_transformer_rnn
 
 
@@ -44,3 +46,64 @@ class CharAwareHybridRNNDecoder(hybrid_transformer_rnn.HybridRNNDecoder):
             finetune_pretrained_weights=finetune_pretrained_weights,
         )
         self.char_layer_norm = nn.LayerNorm(embed_tokens.embedding_dim)
+
+    def _embed_prev_outputs(
+        self, prev_output_tokens, incremental_state=None, prev_output_chars=None
+    ):
+        if incremental_state is not None:
+            prev_output_tokens = prev_output_tokens[:, -1:]
+            if prev_output_chars is not None:
+                prev_output_chars = prev_output_chars[:, -1:, :].squeeze(1)
+
+        x = self.embed_tokens(prev_output_tokens)
+        x = F.dropout(x, p=self.dropout, training=self.training)
+
+        bsz, maxchars = prev_output_chars.size()
+        # char_cnn_encoder takes input (max_word_length, total_words)
+        char_inds_flat = prev_output_chars.view(-1, maxchars).t()
+        # output (total_words, encoder_dim)
+        char_cnn_output = self.char_cnn_encoder(char_inds_flat)
+
+        char_cnn_output = char_cnn_output.view(bsz, 1, char_cnn_output.shape[-1])
+        # (seqlen, bsz, char_cnn_output_dim)
+        char_cnn_output = char_cnn_output.transpose(0, 1)
+        char_cnn_output = self.char_layer_norm(char_cnn_output)
+
+        # B x T x C -> T x B x C
+        x = x.transpose(0, 1)
+
+        return torch.add(x, char_cnn_output), prev_output_tokens
+
+    def forward(
+        self,
+        prev_output_tokens,
+        encoder_out,
+        incremental_state=None,
+        possible_translation_tokens=None,
+        timestep=None,
+        prev_output_chars=None,
+    ):
+        """
+        The assumption is that during inference, the word embedding values are
+        summed with their corresponding character representations. Thus the model
+        will look like the same as a word-based decoder.
+        """
+        if self.training:
+            x, prev_output_tokens = self._embed_prev_outputs(
+                prev_output_tokens=prev_output_tokens,
+                incremental_state=incremental_state,
+                prev_output_chars=prev_output_chars,
+            )
+        else:
+            x, prev_output_tokens = super()._embed_prev_outputs(
+                prev_output_tokens=prev_output_tokens,
+                incremental_state=incremental_state,
+            )
+        return self._forward_given_embeddings(
+            embed_out=x,
+            prev_output_tokens=prev_output_tokens,
+            encoder_out=encoder_out,
+            incremental_state=incremental_state,
+            possible_translation_tokens=possible_translation_tokens,
+            timestep=timestep,
+        )
