ec4660f21599999f386988a7dcd753d510bc2f68
Liz Guo <lizguo@fb.com>
2019-08-25 13:58:00 -0700

    adding BeamSearchAndDecodeV2 class
    Summary:
    The difference between BeamSearchAndDecodeV2 and BeamSearchAndDecode is: V2 calls DecoderBatchedStepEnsemble2BeamWithEOS instead of DecoderBatchedStepEnsemble when
    running beam search. Also, since extra EOS token has been added, it calls BeamDecodeWithEOS when running beam decoding which supports adding extra EOS token.
    
    This class will be merged with upstream later as discussed with James Cross and Halil Akin.
    
    Reviewed By: jmp84, jhcross
    
    Differential Revision: D16939207
    
    fbshipit-source-id: b40efde2950405d31231df7bfd0bbce5ecad22bf

--- pytorch_translate/beam_search_and_decode_v2.py
+++ pytorch_translate/beam_search_and_decode_v2.py
@@ -2,11 +2,17 @@
 
 from typing import List, Tuple
 
+import numpy as np
 import torch
 import torch.jit
 import torch.jit.quantized
 from pytorch_translate.beam_decode import BeamDecode
-from pytorch_translate.ensemble_export import DecoderBatchedStepEnsemble
+from pytorch_translate.ensemble_export import (
+    DecoderBatchedStepEnsemble,
+    EncoderEnsemble,
+    FakeCharSourceEncoderEnsemble,
+    load_models_from_checkpoints,
+)
 from torch import Tensor
 
 
@@ -307,3 +313,282 @@ class BeamDecodeWithEOS(BeamDecode):
             "Dimension of beam_prev_indices : {} and num_steps : {} "
             "are not consistent".format(beam_prev_indices.size(), num_steps)
         )
+
+
+class BeamSearchAndDecodeV2(torch.jit.ScriptModule):
+    """
+    The difference between BeamSearchAndDecodeV2 and BeamSearchAndDecode is: V2 calls
+    DecoderBatchedStepEnsemble2BeamWithEOS instead of DecoderBatchedStepEnsemble when
+    running beam search. Also, since extra EOS token has been added, it calls
+    BeamDecodeWithEOS when running beam decoding which supports adding extra EOS token.
+    """
+
+    def __init__(
+        self,
+        models,
+        tgt_dict,
+        src_tokens,
+        src_lengths,
+        eos_token_id,
+        length_penalty,
+        nbest,
+        beam_size,
+        stop_at_eos,
+        word_reward=0,
+        unk_reward=0,
+        quantize=False,
+    ):
+        super().__init__()
+
+        self.models = models
+        self.tgt_dict = tgt_dict
+        self.beam_size = torch.jit.Attribute(beam_size, int)
+        self.word_reward = torch.jit.Attribute(word_reward, float)
+        self.unk_reward = torch.jit.Attribute(unk_reward, float)
+
+        encoder_ens = EncoderEnsemble(self.models)
+        encoder_ens.enable_precompute_reduced_weights = True
+
+        if quantize:
+            encoder_ens = torch.jit.quantized.quantize_linear_modules(encoder_ens)
+            encoder_ens = torch.jit.quantized.quantize_rnn_cell_modules(encoder_ens)
+
+        # not support char source model
+        self.is_char_source = False
+        enc_inputs = (src_tokens, src_lengths)
+        example_encoder_outs = encoder_ens(*enc_inputs)
+        self.encoder_ens = torch.jit.trace(
+            encoder_ens, enc_inputs, _force_outplace=True
+        )
+        self.encoder_ens_char_source = FakeCharSourceEncoderEnsemble()
+
+        decoder_ens = DecoderBatchedStepEnsemble2BeamWithEOS(
+            self.models,
+            tgt_dict,
+            beam_size,
+            word_reward,
+            unk_reward,
+            tile_internal=False,
+        )
+        decoder_ens.enable_precompute_reduced_weights = True
+        if quantize:
+            decoder_ens = torch.jit.quantized.quantize_linear_modules(decoder_ens)
+            decoder_ens = torch.jit.quantized.quantize_rnn_cell_modules(decoder_ens)
+            decoder_ens = torch.jit.quantized.quantize_rnn_modules(decoder_ens)
+        decoder_ens_tile = DecoderBatchedStepEnsemble2BeamWithEOS(
+            self.models,
+            tgt_dict,
+            beam_size,
+            word_reward,
+            unk_reward,
+            tile_internal=True,
+        )
+        decoder_ens_tile.enable_precompute_reduced_weights = True
+        if quantize:
+            decoder_ens_tile = torch.jit.quantized.quantize_linear_modules(
+                decoder_ens_tile
+            )
+            decoder_ens_tile = torch.jit.quantized.quantize_rnn_cell_modules(
+                decoder_ens_tile
+            )
+            decoder_ens_tile = torch.jit.quantized.quantize_rnn_modules(
+                decoder_ens_tile
+            )
+        prev_token = torch.LongTensor([0])
+        prev_scores = torch.FloatTensor([0.0])
+        ts = torch.LongTensor([0])
+        final_step = torch.tensor([False], dtype=torch.bool)
+        active_hypos = torch.LongTensor([0])
+
+        _, _, _, _, _, *tiled_states = decoder_ens_tile(
+            prev_token, prev_scores, active_hypos, ts, final_step, *example_encoder_outs
+        )
+
+        self.decoder_ens_tile = torch.jit.trace(
+            decoder_ens_tile,
+            (
+                prev_token,
+                prev_scores,
+                active_hypos,
+                ts,
+                final_step,
+                *example_encoder_outs,
+            ),
+            _force_outplace=True,
+        )
+        self.decoder_ens = torch.jit.trace(
+            decoder_ens,
+            (
+                prev_token.repeat(self.beam_size),
+                prev_scores.repeat(self.beam_size),
+                active_hypos.repeat(self.beam_size),
+                ts,
+                final_step,
+                *tiled_states,
+            ),
+            _force_outplace=True,
+        )
+
+        self.beam_decode = BeamDecodeWithEOS(
+            eos_token_id, length_penalty, nbest, beam_size, stop_at_eos
+        )
+
+        self.input_names = [
+            "src_tokens",
+            "src_lengths",
+            "prev_token",
+            "prev_scores",
+            "attn_weights",
+            "prev_hypos_indices",
+            "num_steps",
+        ]
+        self.output_names = [
+            "beam_output",
+            "hypothesis_score",
+            "token_level_scores",
+            "back_alignment_weights",
+            "best_indices",
+        ]
+
+    @torch.jit.script_method
+    def forward(
+        self,
+        src_tokens: torch.Tensor,
+        src_lengths: torch.Tensor,
+        prev_token: torch.Tensor,
+        prev_scores: torch.Tensor,
+        attn_weights: torch.Tensor,
+        prev_hypos_indices: torch.Tensor,
+        active_hypos: torch.Tensor,
+        num_steps: int,
+    ) -> List[Tuple[Tensor, float, List[float], Tensor, Tensor]]:
+
+        enc_states = self.encoder_ens(src_tokens, src_lengths)
+
+        # enc_states ends up being optional because of the above branch, one
+        # side returns None. We should never take the path that returns None
+        # so we unrap the optional type here.
+        enc_states = torch.jit._unwrap_optional(enc_states)
+
+        # remove torch.cat, keep things in a list
+        all_tokens = [prev_token.repeat(repeats=[2 * self.beam_size])]
+        all_scores = [prev_scores.repeat(repeats=[2 * self.beam_size])]
+        all_weights = [
+            attn_weights.unsqueeze(dim=0).repeat(repeats=[2 * self.beam_size, 1])
+        ]
+        all_prev_indices = [prev_hypos_indices]
+
+        prev_token, prev_scores, prev_hypos_indices, attn_weights, active_hypos, *states = self.decoder_ens_tile(
+            prev_token,
+            prev_scores,
+            active_hypos,
+            torch.tensor([0]),
+            torch.tensor([False]),
+            *enc_states,  # noqa
+        )
+        all_tokens = all_tokens.append(prev_token)
+        all_scores = all_scores.append(prev_scores)
+        all_weights = all_weights.append(attn_weights)
+        all_prev_indices = all_prev_indices.append(prev_hypos_indices)
+
+        for i in range(num_steps - 1):
+            (
+                prev_token,
+                prev_scores,
+                prev_hypos_indices,
+                attn_weights,
+                active_hypos,
+                *states,
+            ) = self.decoder_ens(
+                prev_token,
+                prev_scores,
+                active_hypos,
+                torch.tensor([i + 1]),
+                torch.tensor([False]),
+                *states,  # noqa
+            )
+
+            all_tokens = all_tokens.append(prev_token)
+            all_scores = all_scores.append(prev_scores)
+            all_weights = all_weights.append(attn_weights)
+            all_prev_indices = all_prev_indices.append(prev_hypos_indices)
+
+        # add eos token as extra step
+        prev_token, prev_scores, prev_hypos_indices, attn_weights, active_hypos, *states = self.decoder_ens(
+            prev_token,
+            prev_scores,
+            active_hypos,
+            torch.tensor([num_steps]),
+            torch.tensor([True]),
+            *states,
+        )
+
+        all_tokens = all_tokens.append(prev_token)
+        all_scores = all_scores.append(prev_scores)
+        all_weights = all_weights.append(attn_weights)
+        all_prev_indices = all_prev_indices.append(prev_hypos_indices)
+
+        outputs = torch.jit.annotate(
+            List[Tuple[Tensor, float, List[float], Tensor, Tensor]], []
+        )
+        outputs = self.beam_decode(
+            torch.stack(all_tokens, dim=0),
+            torch.stack(all_scores, dim=0),
+            torch.stack(all_weights, dim=0),
+            torch.stack(all_prev_indices, dim=0),
+            num_steps,
+        )
+
+        return outputs
+
+    @classmethod
+    def build_from_checkpoints(
+        cls,
+        checkpoint_filenames,
+        src_dict_filename,
+        dst_dict_filename,
+        beam_size,
+        length_penalty,
+        nbest,
+        word_reward=0,
+        unk_reward=0,
+        lexical_dict_paths=None,
+    ):
+        length = 10
+        models, _, tgt_dict = load_models_from_checkpoints(
+            checkpoint_filenames,
+            src_dict_filename,
+            dst_dict_filename,
+            lexical_dict_paths,
+        )
+        src_tokens = torch.LongTensor(np.ones((length, 1), dtype="int64"))
+        src_lengths = torch.IntTensor(np.array([length], dtype="int32"))
+        eos_token_id = tgt_dict.eos()
+
+        return cls(
+            models,
+            tgt_dict,
+            src_tokens,
+            src_lengths,
+            eos_token_id,
+            length_penalty=length_penalty,
+            nbest=nbest,
+            beam_size=beam_size,
+            stop_at_eos=True,
+            word_reward=word_reward,
+            unk_reward=unk_reward,
+            quantize=True,
+        )
+
+    def save_to_pytorch(self, output_path):
+        def pack(s):
+            if hasattr(s, "_pack"):
+                s._pack()
+
+        def unpack(s):
+            if hasattr(s, "_unpack"):
+                s._unpack()
+
+        self.apply(pack)
+        torch.jit.save(self, output_path)
+        self.apply(unpack)
