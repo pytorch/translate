Fixing wrong dimension for previous characters in char_data (#638)
Summary:
Pull Request resolved: https://github.com/pytorch/translate/pull/638

There was a mistake in dimensions for previous chars in char_data: included one unnecessary additional dimension for previous chars for the last generated words (which are not necessary at all). This causes integration tests break (next diff). Here I fixed that.

Reviewed By: akinh

Differential Revision: D17290436

fbshipit-source-id: 418cdb8a340c50374fff9f2904e1f040cb685847

pytorch_translate/data/char_data.py
@@ -454,7 +454,7 @@ class LanguagePairCharDataset(LanguagePairSourceCharDataset):
         )
         prev_tgt_char_inds = (
             samples[0]["target_chars_list"][0]
-            .new(len(samples), max_tgt_words + 1, max_tgt_word_length)
+            .new(len(samples), max_tgt_words, max_tgt_word_length)
             .long()
             .fill_(self.pad_idx)
         )
@@ -464,7 +464,9 @@ class LanguagePairCharDataset(LanguagePairSourceCharDataset):
             prev_tgt_char_inds[i, 0, :1] = eos_tensor
             for j, chars in enumerate(chars_list):
                 tgt_char_inds[i, j, : tgt_word_lengths[i, j]] = chars
-                prev_tgt_char_inds[i, j + 1, : tgt_word_lengths[i, j]] = chars
+                if j < prev_tgt_char_inds.size()[1] - 1:
+                    # We skip previous characters for the last word.
+                    prev_tgt_char_inds[i, j + 1, : tgt_word_lengths[i, j]] = chars
 
         prev_tgt_word_lengths = torch.cat(
             (torch.ones((len(samples), 1), dtype=torch.long), tgt_word_lengths), dim=1