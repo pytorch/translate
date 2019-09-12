debfc252a0be6b0cac197dae41e1cc5f46610866
Mohammad Sadegh Rasooli <rasooli@fb.com>
2019-09-11 17:26:41 -0700

    Precomputation of character representation for the character-aware decoder (part 1) (#633)
    Summary:
    Pull Request resolved: https://github.com/pytorch/translate/pull/633
    
    After training, the character representations will not change; thus we can precompute and store them in an embedding matrix.
    
    Reviewed By: jhcross
    
    Differential Revision: D16973132
    
    fbshipit-source-id: cc76b3c11ac3982841f346c4576795316eb9153f

--- pytorch_translate/test/test_char_aware_hybrid.py
+++ pytorch_translate/test/test_char_aware_hybrid.py
@@ -7,6 +7,7 @@ from pytorch_translate import char_aware_hybrid, transformer
 from pytorch_translate.data.dictionary import Dictionary
 from pytorch_translate.tasks import pytorch_translate_task as tasks
 from pytorch_translate.test import utils as test_utils
+from pytorch_translate.utils import maybe_cuda
 
 
 class TestCharAwareHybrid(unittest.TestCase):
@@ -45,28 +46,32 @@ class TestCharAwareHybrid(unittest.TestCase):
             arch="hybrid_transformer_rnn"
         )
         self.task = tasks.DictionaryHolderTask(self.word_dict, self.word_dict)
-        word_model = self.task.build_model(test_word_decoder_args)
+        word_model = maybe_cuda(self.task.build_model(test_word_decoder_args))
         word_model.eval()  # Make sure we do not apply dropout.
 
         test_args = test_utils.ModelParamsDict(arch="char_aware_hybrid")
 
-        decoder_embed_tokens = transformer.build_embedding(
-            dictionary=self.word_dict, embed_dim=10
+        decoder_embed_tokens = maybe_cuda(
+            transformer.build_embedding(dictionary=self.word_dict, embed_dim=10)
         )
-        decoder = char_aware_hybrid.CharAwareHybridRNNDecoder(
-            args=test_args,
-            src_dict=self.word_dict,
-            dst_dict=self.word_dict,
-            embed_tokens=decoder_embed_tokens,
-            num_chars=len(self.char_dict),
+        decoder = maybe_cuda(
+            char_aware_hybrid.CharAwareHybridRNNDecoder(
+                args=test_args,
+                src_dict=self.word_dict,
+                dst_dict=self.word_dict,
+                embed_tokens=decoder_embed_tokens,
+                num_chars=len(self.char_dict),
+            )
         )
 
-        src_tokens = self.sample["net_input"]["src_tokens"]
-        src_lengths = self.sample["net_input"]["src_lengths"]
-        prev_output_chars = self.sample["net_input"]["prev_output_chars"][
-            :, -1:, :
-        ].squeeze(1)
-        prev_output_tokens = self.sample["net_input"]["prev_output_tokens"][:, 0:1]
+        src_tokens = maybe_cuda(self.sample["net_input"]["src_tokens"])
+        src_lengths = maybe_cuda(self.sample["net_input"]["src_lengths"])
+        prev_output_chars = maybe_cuda(
+            self.sample["net_input"]["prev_output_chars"][:, -1:, :].squeeze(1)
+        )
+        prev_output_tokens = maybe_cuda(
+            self.sample["net_input"]["prev_output_tokens"][:, 0:1]
+        )
 
         encoder_out = word_model.encoder(src_tokens, src_lengths)
 
@@ -146,6 +151,115 @@ class TestCharAwareHybrid(unittest.TestCase):
         assert output_logits[0].equal(output_logits_shuffled[2])
         assert output_logits[1].equal(output_logits_shuffled[0])
         assert output_logits[2].equal(output_logits_shuffled[1])
+
+    def test_precompute(self):
+        """
+        We test that if we shuffle the input sample, we will get the same
+        forward values, both in training mode (without dropout) and in
+        eval mode.
+        For the meanwhile, we use an auxiliary hybrid_transformer_rnn
+        in order to get the encoder output.
+        """
+        test_args = test_utils.ModelParamsDict(arch="char_aware_hybrid")
+
+        decoder_embed_tokens = maybe_cuda(
+            transformer.build_embedding(dictionary=self.word_dict, embed_dim=10)
+        )
+
+        decoder = maybe_cuda(
+            char_aware_hybrid.CharAwareHybridRNNDecoder(
+                args=test_args,
+                src_dict=self.word_dict,
+                dst_dict=self.word_dict,
+                embed_tokens=decoder_embed_tokens,
+                num_chars=len(self.char_dict),
+            )
+        )
+        # Making sure we do not apply dropout.
+        decoder.eval()
+        decoder.precompute_char_representations(
+            char_dict=self.char_dict, embed_bytes=False, batch_size=5
+        )
+
+        first_embedding = decoder.combined_word_char_embed.weight.clone()
+        first_embedding.detach()
+        decoder.precompute_char_representations(
+            char_dict=self.char_dict, embed_bytes=False, batch_size=11
+        )
+        second_embedding = decoder.combined_word_char_embed.weight.clone()
+        second_embedding.detach()
+
+        # Due to a known issue in the char_encoder model, this does not hold for
+        # torch.equal (T53048656)
+        assert torch.allclose(first_embedding, second_embedding, rtol=1e-04, atol=1e-04)
+
+        decoder.precompute_char_representations(
+            char_dict=self.char_dict, embed_bytes=False, batch_size=23
+        )
+        third_embedding = decoder.combined_word_char_embed.weight.clone()
+        third_embedding.detach()
+        # Due to a known issue in the char_encoder model, this does not hold for
+        # torch.equal (T53048656)
+        assert torch.allclose(second_embedding, third_embedding, rtol=1e-04, atol=1e-04)
+
+    def test_forward_training_precompute(self):
+        """
+        We test if precomputation gives the same result.
+        """
+        test_args = test_utils.ModelParamsDict(arch="char_aware_hybrid")
+
+        decoder_embed_tokens = transformer.build_embedding(
+            dictionary=self.word_dict, embed_dim=10
+        )
+        decoder = maybe_cuda(
+            char_aware_hybrid.CharAwareHybridRNNDecoder(
+                args=test_args,
+                src_dict=self.word_dict,
+                dst_dict=self.word_dict,
+                embed_tokens=decoder_embed_tokens,
+                num_chars=len(self.char_dict),
+            )
+        )
+        decoder.eval()
+        prev_output_word_strs = self.word_dict.symbols[-3:]
+        prev_out_word_indices = [
+            self.word_dict.indices[w] for w in prev_output_word_strs
+        ]
+        prev_output_tokens = maybe_cuda(
+            torch.LongTensor(prev_out_word_indices).unsqueeze(1)
+        )
+
+        prev_output_chars = maybe_cuda(
+            torch.LongTensor(
+                [
+                    decoder._char_list_for_word(
+                        word_index=word_index, word=word, char_dict=self.char_dict
+                    )
+                    for word_index, word in zip(
+                        prev_out_word_indices, prev_output_word_strs
+                    )
+                ]
+            )
+        )
+
+        embed_output = maybe_cuda(
+            decoder._embed_prev_outputs(
+                prev_output_tokens=prev_output_tokens,
+                prev_output_chars=prev_output_chars,
+            )[0]
+        )
+
+        decoder.precompute_char_representations(
+            char_dict=self.char_dict, embed_bytes=False, batch_size=30
+        )
+        embed_output_after_precompute = decoder._embed_prev_outputs(
+            prev_output_tokens=prev_output_tokens, prev_output_chars=prev_output_chars
+        )[0]
+        # Due to a known issue in padding, we know that the results are not exactly
+        # the same.
+        assert torch.allclose(
+            embed_output, embed_output_after_precompute, rtol=1e-04, atol=1e-04
+        )
 
     def _dummy_char_data_sample(
         self,
