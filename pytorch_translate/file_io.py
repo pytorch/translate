58bd5485e43f8d1a252cf0f4e6ef49d4f32a409a
Wei Ho <weiho@fb.com>
2019-12-05 18:12:58 -0800

    Add wrapper abstraction on top of fvcore PathManager to insulate OSS compatibility
    Reviewed By: myleott
    
    Differential Revision: D18736914
    
    fbshipit-source-id: 897ba7463f7a8ebc0969c01c57bfe04fbf9e71c8

--- pytorch_translate/file_io.py
+++ pytorch_translate/file_io.py
@@ -4,48 +4,12 @@
 TODO(T55884145): Deprecate this in favor of using
 fvcore.common.file_io.PathManager directly.
 """
-import os
-from typing import List
+from fairseq.file_io import PathManager  # noqa
 
 
 try:
-    from fvcore.common.file_io import PathManager
     from manifold.clients.python import StorageException
-
 except (ImportError, ModuleNotFoundError):
-
-    class PathManager:
-        @staticmethod
-        def open(path: str, mode: str = "r"):
-            return open(path, mode)
-
-        @staticmethod
-        def copy(src_path: str, dst_path: str, overwrite: bool = False) -> bool:
-            raise NotImplementedError()
-
-        @staticmethod
-        def get_local_path(path: str) -> str:
-            raise NotImplementedError()
-
-        @staticmethod
-        def exists(path: str) -> bool:
-            return os.path.exists(path)
-
-        @staticmethod
-        def isfile(path: str) -> bool:
-            return os.path.isfile(path)
-
-        @staticmethod
-        def ls(path: str) -> List[str]:
-            return os.listdir(path)
-
-        @staticmethod
-        def mkdirs(path: str):
-            os.makedirs(path, exist_ok=True)
-
-        @staticmethod
-        def rm(path: str):
-            os.remove(path)
 
     class StorageException(Exception):
         pass
