5fe64ae6c74b6536db058569ad6cdf35680e493f
Wei Ho <weiho@fb.com>
2020-01-31 10:40:29 -0800

    Add support for upload_local_path & improve exception logging
    Summary:
    * Add support for `upload_local_path()` (parallels `get_local_path()`)
    * Improve exception logging per https://fburl.com/s8n7pmh2
    
    Reviewed By: wanyenlo, takatosp1
    
    Differential Revision: D19452722
    
    fbshipit-source-id: 65a65f284d8fe63427678faf663137d9084dc792

--- pytorch_translate/file_io.py
+++ pytorch_translate/file_io.py
@@ -5,11 +5,3 @@ TODO(T55884145): Deprecate this in favor of using
 fvcore.common.file_io.PathManager directly.
 """
 from fairseq.file_io import PathManager  # noqa
-
-
-try:
-    from manifold.clients.python import StorageException
-except (ImportError, ModuleNotFoundError):
-
-    class StorageException(Exception):
-        pass
