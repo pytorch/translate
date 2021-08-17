73c8f848a220377b03a2eba077de363ddb502c14
Zsolt Dollenstein <zsol@fb.com>
2021-08-17 06:06:00 -0700

    Enable BLACK by default
    Reviewed By: zertosh
    
    Differential Revision: D30366280
    
    fbshipit-source-id: 45bdf51abc640531ba66c5f7cdc9aba94efbe70f

--- setup.py
+++ setup.py
@@ -4,29 +4,29 @@ from setuptools import setup, find_packages
 
 
 def readme():
-    with open('README.md') as f:
+    with open("README.md") as f:
         return f.read()
 
 
 def requirements():
-    with open('requirements.txt') as f:
+    with open("requirements.txt") as f:
         return f.read()
 
 
 setup(
-    name='pytorch-translate',
-    version='0.1',
-    author='Facebook AI',
-    description=('Facebook Translation System'),
+    name="pytorch-translate",
+    version="0.1",
+    author="Facebook AI",
+    description=("Facebook Translation System"),
     long_description=readme(),
-    url='https://github.com/pytorch/translate',
-    license='BSD',
+    url="https://github.com/pytorch/translate",
+    license="BSD",
     packages=find_packages(),
     install_requires=[
-        'fairseq>=0.5.0',
+        "fairseq>=0.5.0",
     ],
     dependency_links=[
-        'git+https://github.com/pytorch/fairseq.git#egg=fairseq-0.5.0',
+        "git+https://github.com/pytorch/fairseq.git#egg=fairseq-0.5.0",
     ],
-    test_suite='pytorch_translate',
+    test_suite="pytorch_translate",
 )
